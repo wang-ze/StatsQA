bd3c19728f02c7146f49f2b7e129a85891b9561b
Ze Wang <zewang@gmail.com>
2024-07-01 11:32:56 -0400

    update json string bug

--- src/STATSQAGenerator/STATSQAGenerator.py
+++ src/STATSQAGenerator/STATSQAGenerator.py
@@ -1,64 +1,71 @@
 import os
 import json
-import traceback
 import pandas as pd
 from dotenv import load_dotenv
-from src.mcqgenerator.utils import read_file,get_table_data
-from src.mcqgenerator.logger import logging
+from src.STATQAGenerator.utils import read_file,get_table_data
+from src.STATQAGenerator.logger import logging
 
 #imporing necessary packages packages from langchain
-from langchain.chat_models import ChatOpenAI
-from langchain.prompts import PromptTemplate
-from langchain.chains import LLMChain
-from langchain.chains import SequentialChain
+from operator import itemgetter
+from langchain_core.prompts import PromptTemplate
+# from langchain_core.output_parsers import StrOutputParser
+from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+from langchain_groq import ChatGroq
 
 
 # Load environment variables from the .env file
-load_dotenv()
+load_dotenv() 
 
 # Access the environment variables just like you would with os.environ
 key=os.getenv("OPENAI_API_KEY")
 
 
-llm = ChatOpenAI(openai_api_key=key,model_name="gpt-3.5-turbo", temperature=0.7)
+model = ChatGroq(groq_api_key= KEY, model="llama3-8b-8192")
 
-template="""
+TEMPLATE="""
 Text:{text}
-You are an expert MCQ maker. Given the above text, it is your job to \
-create a quiz  of {number} multiple choice questions for {subject} students in {tone} tone. 
-Make sure the questions are not repeated and check all the questions to be conforming the text as well.
-Make sure to format your response like  RESPONSE_JSON below  and use it as a guide. \
-Ensure to make {number} MCQs
+You are an expert STATSQA maker. Given the above text, it is your job to \
+create a quiz of {number} multiple choice questions related to {area} for students in {grade}. 
+Make sure the questions are not repeated and format your response like RESPONSE_JSON below and use it as a guide. \
+Ensure to generate {number} questions.
 ### RESPONSE_JSON
-{response_json}
+{response_json} \
 
 """
-
-quiz_generation_prompt = PromptTemplate(
-    input_variables=["text", "number", "subject", "tone", "response_json"],
-    template=template)
-
-
-quiz_chain=LLMChain(llm=llm,prompts=quiz_generation_prompt,output_key="quiz",verbose=True)
-
-
-template2="""
-You are an expert english grammarian and writer. Given a Multiple Choice Quiz for {subject} students.\
-You need to evaluate the complexity of the question and give a complete analysis of the quiz. Only use at max 50 words for complexity analysis. 
-if the quiz is not at per with the cognitive and analytical abilities of the students,\
-update the quiz questions which needs to be changed and change the tone such that it perfectly fits the student abilities
-Quiz_MCQs:
-{quiz}
+TEMPLATE2="""
+You are an expert of English grammar. \
+You are given a STATSQA quiz: {quiz} of multiple choice questions related to {area} in statistics.\
+You need to evaluate the complexity of the quiz. Use at most 50 words for complexity analysis. \
+If the quiz is too easy or too difficult for students in {grade}, 
+update the quiz questions to make it more suitable for the students in {grade}.
 
 Check from an expert English Writer of the above quiz:
 """
 
+quiz_generation_prompt = PromptTemplate(
+    template=TEMPLATE, 
+    input_variables=["text", "number", "area", "grade", "response_json"]
+)
 
-quiz_evaluation_prompt=PromptTemplate(input_variables=["subject", "quiz"], template=template2)
+#%% 
+# quiz_generation_prompt = PromptTemplate.from_template(
+#     template=TEMPLATE
+# )
 
-review_chain=LLMChain(llm=llm, prompt=quiz_evaluation_prompt, output_key="review", verbose=True)
+quiz_evaluation_prompt = PromptTemplate.from_template(TEMPLATE2)
+
+quiz_generation_chain= quiz_generation_prompt | model
+quiz_evaluation_chain = quiz_evaluation_prompt | model
 
 
-# This is an Overall Chain where we run the two chains in Sequence
-generate_evaluate_chain=SequentialChain(chains=[quiz_chain, review_chain], input_variables=["text", "number", "subject", "tone", "response_json"],
-                                        output_variables=["quiz", "review"], verbose=True,)
+complete_chain = ({
+    "text": itemgetter("text"),
+    "number": itemgetter("number"),
+    "area": itemgetter("area"),
+    "grade": itemgetter("grade"),
+    "response_json": itemgetter("response_json"),
+    "quiz": quiz_generation_chain
+    }
+    | RunnablePassthrough.assign(eval=quiz_evaluation_chain)
+)
+
--- src/STATSQAGenerator/utils.py
+++ src/STATSQAGenerator/utils.py
@@ -24,24 +24,43 @@ def read_file(file):
             "unsupported file format only pdf and text file suppoted"
             )
 
-def get_table_data(quiz_str):
+
+def extract_between_braces(s):
+    start = s.find('{')
+    # Ensure the character after the first '{' is not another '{'
+    while start != -1 and start + 1 < len(s) and s[start + 1] == '{':
+        start = s.find('{', start + 1)
+    
+    end = s.rfind('}')
+    # Ensure the character before the last '}' is not another '}'
+    while end != -1 and end - 1 >= 0 and s[end - 1] == '}':
+        end = s.rfind('}', 0, end - 1)
+    
+    if start != -1 and end != -1 and end > start:
+        return "{" + s[start+1:end] + "}}"
+    return ""
+
+
+
+def get_table_data(all_result):
     try:
+        quiz_str = all_result.get("quiz").content
+        quiz_str = extract_between_braces(quiz_str)
         # convert the quiz from a str to dict
         quiz_dict=json.loads(quiz_str)
         quiz_table_data=[]
         
         # iterate over the quiz dictionary and extract the required information
         for key,value in quiz_dict.items():
-            mcq=value["mcq"]
-            options=" || ".join(
+            STATSQA = value["STATSQA"]
+            options = " | ".join(
                 [
-                    f"{option}-> {option_value}" for option, option_value in value["options"].items()
-                 
-                 ]
-            )
-            
-            correct=value["correct"]
-            quiz_table_data.append({"MCQ": mcq,"Choices": options, "Correct": correct})
+                    f"{option}: {option_value}"
+                    for option, option_value in value["options"].items()
+                    ]
+        )
+            correct = value["correct"]
+            quiz_table_data.append({"STATSQA": STATSQA, "Choices": options, "Correct": correct})
         
         return quiz_table_data
         
